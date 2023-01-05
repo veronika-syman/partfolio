90acc0af0477550e6335cbe1c600e28f82a93e7d
veronika-syman <116827488+veronika-syman@users.noreply.github.com>
2023-01-05 11:47:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/*****************************
+ * 
+ *  SMOOTH SCROLL TO ANCHOR
+ * 
+ */
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        // const topOffset = document.querySelector('.top-offset').offsetHeight;
+        const topOffset = 0; // нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
